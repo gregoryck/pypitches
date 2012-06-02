c1b893285e92996d1f96a03c96a07dd1a1842f4f
Greg Kettler <gkettler@alum.mit.edu>
2012-06-02 12:42:57 -0500

    Added a distutils setup.py and moved everything around to go with it

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup (
+        name = "PyPitches",
+        version = "0.1.0",
+        author = "Greg Kettler",
+        author_email = "gkettler@gmail.com",
+        packages=['pypitches', 'pypitches.test'],
+        #scripts=[],
+        url="http://thefamilyatomics.com/pypitches",
+        description="Analyzing Major League Baseball pitch data from PITCHf/x",
+        long_description=open('README.txt').read(),
+        requires=[
+            "SQLAlchemy (>=0.7)",
+            "BeautifulSoup (>=3.2.0)",
+            "matplotlib (>=1.0.0)",
+            "numpy (>=1.6.1)",
+            ],
+        )
+
