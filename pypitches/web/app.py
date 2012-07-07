53148f51f58ae52ed3e965eb92fe46d7d25ea422
Greg Kettler <gkettler@alum.mit.edu>
2012-07-07 00:01:32 -0500

    Setting up layout for webdev stuff

--- pypitches/web/app.py
+++ pypitches/web/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, session, url_for, render_template, flash
+from flask import Flask, request, session, url_for, render_template, flash, send_from_directory
 from pypitches import load
+import traceback
+from os.path import join
 
 app = Flask(__name__)
 
 
-@app.route('/pypitches/')
+@app.route('/pypitches/status')
 def status():
     return render_template("status.html")
 
@@ -14,8 +16,18 @@ def controls():
 
 @app.route('/pypitches/load', methods=['POST'])
 def load():
-    gamedir_id = reqest.form['gamedir_id']
+    gamedir_id = request.form['gamedir_id']
 
+@app.route('/static/<filename>')
+def send_foo(filename):
+    print "hi!", filename
+    print app.root_path
+    try:
+        return send_from_directory(join(app.root_path, 'static'), filename)
+    except:
+        print "hey!"
+        traceback.print_exc()
+        raise
 
 
 if __name__ == "__main__":
