8dd16ca68dc38c2915ab385e1a44a5db1e94f9c4
Greg Kettler <gkettler@Abh.(none)>
2013-03-31 22:47:08 -0500

    toward a test of the database queries

--- pypitches/test/test_plots.py
+++ pypitches/test/test_plots.py
@@ -48,52 +48,56 @@ class TestPlots(TestCase):
         atbat = pitch.atbat
         self.assertEqual(atbat.game, pitch.game)
 
-# class TestDB(TestCase):
-#     def setUp(self):
-#         self.game = createGame()
-#         self.atbat = createAtBat(self.game)
-#         self.pitch = createPitch(self.atbat)
-
-# game_pk = 1
-# def createGame():
-#     conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
-#     sql = """
-#           INSERT INTO game
-#           (game_pk, away_team_code, home_team_code, away_fname, home_fname, away_sname, home_sname, date)
-#           VALUES
-#           (%d, 'BOS', 'CHA', 'Boston Red Sox', 'Chicago White Sox', '', '', '2013-04-01')
-#           """
-#     cursor.execute(sql, game_pk )
-#     game_pk += 1
-#     return game_pk - 1
-
-# playerid = 1
-# def createPlayer():
-#     sql = """
-#           INSERT INTO player
-#           (id, first, last)
-#           VALUES
-#           (%d, 'Testy', 'Testerson')
-#           """
-#     conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
-#     cursor.execute(sql, playerid)
-#     playerid  += 1
-#     return playerid - 1
-
-# atbatnum = 1
-# def createAtBat(game, pitcher, batter):
-#     sql = """
-#           INSERT INTO atbat
-#           (inning, num, b, s, batter, stand, p_throws, pitcher, des, event, brief_event, game_pk, date)
-#           VALUES
-#           (1, %d, 1, 1, %d, 'R', 'R', %d, 'batted ball hits a dove', 'single', 'single', %d, '2013-04-01')
-#           """
-#     conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
-#     cursor.execute(sql, batter.id, pitcher.id, game.game_pk)
-#     atbatnum += 1
-#     return atbatnum - 1
+class TestDB(TestCase):
+    def setUp(self):
+        self.game = createGame()
+        self.atbat = createAtBat(self.game)
+        self.batter = createPlayer()
+        self.pitcher = createPlayer()
+        self.atbat = createAtBat(self.game, self.pitcher, self.batter)
+        self.pitch = createPitch(self.atbat)
 
 
 
-if __name__ == "__main__":
-    nose.main()
+game_pk = 1
+def createGame():
+    global game_pk
+    conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
+    sql = """
+          INSERT INTO game
+          (game_pk, away_team_code, home_team_code, away_fname, home_fname, away_sname, home_sname, date)
+          VALUES
+          (%d, 'BOS', 'CHA', 'Boston Red Sox', 'Chicago White Sox', '', '', '2013-04-01')
+          """
+    cursor.execute(sql, game_pk )
+    game_pk += 1
+    return game_pk - 1
+
+playerid = 1
+def createPlayer():
+    global playerid
+    sql = """
+          INSERT INTO player
+          (id, first, last)
+          VALUES
+          (%d, 'Testy', 'Testerson')
+          """
+    conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
+    cursor.execute(sql, playerid)
+    playerid  += 1
+    return playerid - 1
+
+atbatnum = 1
+def createAtBat(game, pitcher, batter):
+    global atbatnum
+    sql = """
+          INSERT INTO atbat
+          (inning, num, b, s, batter, stand, p_throws, pitcher, des, event, brief_event, game_pk, date)
+          VALUES
+          (1, %d, 1, 1, %d, 'R', 'R', %d, 'batted ball hits a dove', 'single', 'single', %d, '2013-04-01')
+          """
+    conn, cursor = get_cursor(postgres_test_db, postgres_user, postgres_password)
+    cursor.execute(sql, batter.id, pitcher.id, game.game_pk)
+    atbatnum += 1
+    return atbatnum - 1
+
